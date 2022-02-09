afd7faf064058f0856c220d131249434f37d3301
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-02-09 12:47:19 +0300

    rendering of completed tasks

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -4,15 +4,24 @@ import {useSelector} from 'react-redux';
 import {TodoItem} from '../TodoItem';
 import {style} from './style';
 import {StartWindow} from '../StartWindow';
-import {todoItemsSelectors} from '../../redux/selectors/todoSelector';
+import {
+  doneItemsSelectors,
+  todoItemsSelectors,
+} from '../../redux/selectors/todoSelector';
 import {ReduxStoreType} from '../../redux/store';
 import {TodoItemType} from '../../models';
 
-export const ItemList: React.FC = () => {
+export const ItemList: React.FC = ({route, navigation}) => {
+  const flagDone = route.params.done;
+  console.log('FLAG', flagDone);
   const todoItems = useSelector<ReduxStoreType, TodoItemType[]>(
     todoItemsSelectors,
   );
-  console.log('ITEM', todoItems);
+  const doneItems = useSelector<ReduxStoreType, TodoItemType[]>(
+    doneItemsSelectors,
+  );
+  const data = flagDone ? doneItems : todoItems;
+  console.log('ITEM', data);
 
   const keyExtractor: FlatListProps<TodoItemType>['keyExtractor'] = item => {
     return item.id;
@@ -25,7 +34,7 @@ export const ItemList: React.FC = () => {
   return (
     <View style={style.container}>
       <FlatList
-        data={todoItems}
+        data={data}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
         ListEmptyComponent={<StartWindow />}
--- src/redux/selectors/todoSelector.ts
+++ src/redux/selectors/todoSelector.ts
@@ -3,3 +3,7 @@ import {ReduxStoreType} from '../store';
 export const todoItemsSelectors = (state: ReduxStoreType) => {
   return state.todoItems;
 };
+
+export const doneItemsSelectors = (state: ReduxStoreType) => {
+  return state.doneItems;
+};
